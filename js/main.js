f3fcf5ad9c2d8154ad8292a3cf55db150be549ba
Elaine Esmenda <elaineesmenda@Elaine.local>
2020-10-01 13:22:09 -0400

    adding SASS stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // import your packages here 
 import Person from "./modules/DataModule.js";
-import NavSystem from "./modules/TheNavSystem.js"
 
 (() => {
     // stub * a place for component specific stuff
--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     name: "Elaine", 
     role: "Student",
     nickname: "Leyn"
