5e6d7a923bcdc074b847ba1db37d8d608ea701cd
Elaine Esmenda <elaineesmenda@Elaine.local>
2020-11-05 16:19:28 -0500

    fixing errors

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// import your packages here
-import { fetchData } from "./modules/TheDataMiner.js";
+import {fetchData} from "./modules/TheDataMiner.js";
 
 (() => {
     // stub * just a place for non-component-specific stuff
@@ -27,30 +26,29 @@ import { fetchData } from "./modules/TheDataMiner.js";
         }
     }
 
-    function retrieveProjectInfo() {
+    function retrieveProjectInfo () {
         // test for an ID
-        debugger;
         console.log(this.id);
 
         fetchData(`./includes/index.php?id=${this.id}`).then(data => console.log(data)).catch(err => console.log(err));
     }
 
     function renderPortfolioThumbnails(thumbs) {
-        debugger;
         let userSection = document.querySelector('.user-section'),
             userTemplate = document.querySelector('#user-template').content;
-
-        for (let user in thumbs) {
+        
+        for (let user in thumbs) {  
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.user').children;
 
-            currentUserText[1].src = `images/${thumbs[user].avatar}`;
-            currentUserText[1].id = thumbs[user].id;
+             currentUserText[1].src = `images/${thumbs[user].avatar}`;
+             currentUserText[1].id = thumbs[user].id;
             // add this new user to the view
-            currentUser.addEventListener("click", retrieveProjectInfo);
-            userSection.appendChild(currentUser);
-        }
+             currentUser.addEventListener("click", retrieveProjectInfo);
+             userSection.appendChild(currentUser);
+         }
     }
-        
+
     fetchData("./includes/index.php").then(data => renderPortfolioThumbnails(data)).catch(err => console.log(err));
+    // fetchData("./includes/functions.php").then(data => handleDataSet(data)).catch(err => console.log(err));
 })();
